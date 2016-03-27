f10a47dbb03cde7dd4ad0b7d3691e3c3711679b4
Samuel Waker <ldspcjunkie@gmail.com>
2016-03-26 20:09:52 -0600

    added furniture class

---
+++ Assets/data/Furniture.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System;
+
+public class Furniture {
+
+    Tile tile;
+    string furnitureType;
+    float movementCost;
+    Action<Furniture> onUpdate;
+
+    public Dictionary<string, System.Object> variables = new Dictionary<string, System.Object>();
+    public void update(){
+        if(onUpdate != null)
+            onUpdate(this);
+    }
+
+    public Furniture(string furnitureType, float movementCost, Action<Furniture> onUpdate) {
+        this.furnitureType = furnitureType;
+        this.movementCost = movementCost;
+        this.onUpdate = onUpdate;
+    }
+
+    public Furniture(Furniture prefab, Tile tile) {
+        this.tile = tile;
+        this.furnitureType = prefab.furnitureType;
+        this.movementCost = prefab.movementCost;
+        this.onUpdate = prefab.onUpdate;
+    }
+
+}
